edfa5d075dfba94131257e42a94e09df974794be
liuylin <1272894923@qq.com>
2018-01-13 14:10:07 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<assert.h>
+using namespace std;
+#include"huffuman.h"
+#include"FileCompress.h"
+
+
+
+int main()
+{
+	//TestHuffumanTree();
+	TestFileCompess();
+	//Test();
+	system("pause");
+	return 0;
+}
